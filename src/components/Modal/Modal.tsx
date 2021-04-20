7af5125b7255610ba95ba0fd913d2b09b3fdc4c7
MrNithi <bossrocker777@gmail.com>
2021-04-20 20:02:28 +0700

    Fix click Modal backdrop to hide it

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,9 +9,9 @@ type Props = {
 const Modal: React.FC<Props> = ({ title, children, onDismiss }) => {
   return (
     <>
-      <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50">
+      <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0">
         <div className="mx-auto max-w-3xl">
-          <div className="flex flex-col rounded-3xl shadow-lg bg-white dark:bg-gray-darkest outline-none focus:outline-none">
+          <div className="flex flex-col rounded-3xl shadow-lg bg-white dark:bg-gray-darkest outline-none focus:outline-none relative z-50">
             <div className="flex items-start justify-between p-5 border-b border-solid border-gray rounded-t">
               <h3 className="text-2xl dark:text-gray-light font-semibold">{title}</h3>
               <button
@@ -23,9 +23,9 @@ const Modal: React.FC<Props> = ({ title, children, onDismiss }) => {
             </div>
             <div className="flex p-6">{children}</div>
           </div>
+          <div className="fixed inset-0 z-40 bg-black opacity-50" onClick={onDismiss} />
         </div>
       </div>
-      <div className="fixed inset-0 z-40 bg-black opacity-50" onClick={onDismiss} />
     </>
   )
 }
