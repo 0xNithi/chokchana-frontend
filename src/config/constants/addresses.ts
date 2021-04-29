7e83327718687310c642078b3f70920918495c25
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-29 13:19:05 +0700

    redeploy smart contract once again ;-;

--- src/config/constants/addresses.ts
+++ src/config/constants/addresses.ts
@@ -1,4 +1,4 @@
 export const THBTokenAddress = '0x95bd962428294935Bf39508995aB9f204e5bf00a'
-export const TicketAddress = '0x78C1889cD0F5dbf5237Cecc2976f657A2B6B8432'
-export const LotteryAddress = '0x3Ed505Cb5444DC4a34a69Ca32211C8243a26c369'
-export const RandomGeneratorAddress = '0x44734AD3D1FC9133BE00E91252A215E53fcAfbc6'
+export const TicketAddress = '0x3FE1fF2658896bDCbBbf207344E4296381A61511'
+export const LotteryAddress = '0xE6EE8FeFEAC57659f728F442BFcA88cEC6fFA489'
+export const RandomGeneratorAddress = '0x363B1f9eA24ed2AC25693E08EEA7f1F9a0CaB80E'
