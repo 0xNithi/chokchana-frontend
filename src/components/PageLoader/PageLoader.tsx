4977fa9ca5948ca27eccb11131115b73f9a1543a
MrNithi <bossrocker777@gmail.com>
2021-05-02 23:32:37 +0700

    Update loading page and not found page

--- src/components/PageLoader/PageLoader.tsx
+++ src/components/PageLoader/PageLoader.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const PageLoader: React.FC = () => {
-  return <div className="flex justify-center items-center h-full">loading</div>
+  return <div className="flex flex-col justify-center items-center h-full text-5xl text-purple-light">
+    <img src="/images/loading.png" alt="loading" />
+    กำลังโหลด...
+  </div>
 }
 
 export default PageLoader
